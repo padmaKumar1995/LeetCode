e3491e261fa153e15aacc4ca2684911de3e64aee
Padmakumar <96547479+padmaKumar1995@users.noreply.github.com>
2023-08-29 18:32:29 +0530

    Time: 1 ms, Memory: 40.8 MB - LeetHub

---
+++ jewels-and-stones/jewels-and-stones.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        
+        for(char c: jewels.toCharArray()) {
+            set.add(c);
+        }
+        
+        int ans = 0;
+        for(char c: stones.toCharArray()) {
+            if(set.contains(c)) {
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
